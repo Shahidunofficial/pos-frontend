b107a2484e56bfa3397915ecc1c88baba5b00d26
ShahidA <86820035+Shahidunofficial@users.noreply.github.com>
2025-07-13 09:42:58 +0530

    Update config.ts

--- src/API/config.ts
+++ src/API/config.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'https://pos-backend-oaym.onrender.com/';
+export const API_BASE_URL = 'https://pos-backend-oaym.onrender.com';
 
 export async function makeRequest<T>(
   endpoint: string,
